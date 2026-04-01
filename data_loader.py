af44c2beef9ce9f6dd9af39ba2c2f948b674e378
@Masum0895 <2221793@iub.edu.bd>
2026-04-01 18:09:53 +0600

    Initial commit: RAG application with FastAPI, Inngest, and Qdrant

---
+++ data_loader.py
@@ -0,0 +1,8 @@
+from openai import OpenAI
+from llama_index.readers.file import PDFReader
+from llama_index.core.node_parser import SentenceSplitter
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI()
